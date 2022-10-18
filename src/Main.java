4580478450d6579f772e2d7711103a3397cc3591
Yavuzhan Terzi <yavuzhanterzi@gmail.com>
2022-10-18 04:14:24 +0300

    Ders2_Odev

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] sayilar = new int[]{1, 2, 5, 7, 9, 0};
+        int aranacak = 5;
+        boolean varMi = false;
+
+        for (int sayi23 : sayilar) {
+            if (sayi23 == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+        if (varMi) {
+            System.out.println("sayı mevcuttur");
+        } else {
+            System.out.println("sayı mevcut değildir");
+        }
+
+
+
+
     }
-
-
-
-
 }
